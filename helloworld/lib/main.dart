9980a5f79d94acbf6da488aa6b817e11da4580b4
lyomu <gmnyomu@gmail.com>
2023-03-05 02:29:59 +0300

    Scaffold items

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(
+void main()  {
+  runApp(
     MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
+        appBar: AppBar(
+          title: Text('Scraffold Appbar'),
+        ),
+        backgroundColor: Colors.lightBlue,
+        body:Center(child:
+          Text('Scaffold Widget Example'),
+          ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('T'),
+          onPressed: () {},
+        ),
+        drawer: Drawer(
+          child: Text('Drawer'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              label: 'Home',
+                icon: Icon(Icons.home)),
+            BottomNavigationBarItem(
+              label: 'AC Unit',
+                icon: Icon(Icons.ac_unit),),
+          ],
         ),
       ),
-));
+    )
+
+  );
+}
+
